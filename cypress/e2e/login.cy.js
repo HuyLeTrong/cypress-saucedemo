682c0535e559c7d3b8746a00531a22aea83a1ca2
TestHLT <106044424+HuyLeTrong@users.noreply.github.com>
2026-05-07 16:57:56 +0700

    Update login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -92,31 +92,5 @@ describe('Saucedemo Login', () => {
         })
     });
 
-    // it('saucedemo-verify-specific-item-details (wrong case for screenshot)', () => {
-    // const src1 = '/static/media/sauce-backpack-1200x1500.0a0b85a385945026062b.jpg';
-    // cy.get('[data-test="username"]').click();
-    // cy.get('[data-test="username"]').type('visual_user');
-    // cy.get('[data-test="password"]').click();
-    // cy.get('[data-test="password"]').type('secret_sauce');
-    // cy.get('[data-test="login-button"]').click();
-    // cy.contains('.inventory_item', 'Sauce Labs Backpack')
-    //     .within(() => {
-    //         // Check name
-    //         cy.get('.inventory_item_name')
-    //             .should('have.text', 'Sauce Labs Backpack')
-            
-    //         // Check price, $29.99 is the correct price, so we check not equal to confirm price is wrong
-    //         cy.get('.inventory_item_price').then(($price) => {
-    //             const priceText = $price.text().trim();
-    //             expect(priceText).to.equal('$29.99')
-    //         })
-
-
-    //         // Check image has correct src
-    //         cy.get('.inventory_item_img img').invoke('attr', 'src').then((nextSrc) => {
-    //             expect(nextSrc).to.not.equal(src1)
-    //         }) //src1 is the correct image, compare to not equal to confirm the image is wrong
-    //     })
-    // });
 });
 
